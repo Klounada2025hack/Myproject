67bb2496b63ede5b0fca926986614527fab375ff
Klounada2025hack <dark_0n@mail.ru>
2025-12-24 02:14:16 +0300

    Дописана игра и исправлены баги

--- src/gameLevkin.cs
+++ src/gameLevkin.cs
@@ -1,85 +1,138 @@
-﻿using System.Xml.Linq;
+﻿using System;
+using System.Xml.Linq;
 
 Console.WriteLine("Ты просыпаешься в темной-темной комнате и тут голос из угла комнаты спрашивает: Как тебя зовут? \nНазовись...");
 string name = Console.ReadLine();
+if (!string.IsNullOrEmpty(name))
+{
+    name = char.ToUpper(name[0]) + name.Substring(1);
+}
+Console.WriteLine("------------------------------");
 Console.WriteLine("Голос, который только что тебя спрашивал, заговорил вновь: \"Попробуй выберись! ХАХАХААХ...\"\nПривыкнув к темноте, ты замечаешь, что тут много мебели...");
+Console.WriteLine("------------------------------");
+
 bool game = false;
-bool first_art = false;
-bool second_art = false;
-bool third_art = false;
-bool chetverty = false;
-bool chetverty_art = false;
+
+bool konvert = false;
+bool karandash = false;
+bool metla = false;
+bool polotentse = false;
+bool lom = false;
+bool shitokVkluchen = false;
+bool lighterSdelan = false;
+bool folgaIBatareykiNaydeny = false;
+
 bool check = false;
 bool check2 = false;
 int schetchik = 0;
+bool narisoval = false;
+bool ventilyatsiyaOtkryta = false;
 
-while (game == false)
+while (!game)
 {
-    if (first_art && second_art && third_art&&chetverty_art)
+    if (konvert && karandash && metla && lom)
     {
+        Console.WriteLine("------------------------------");
         Console.WriteLine("Голос извне вновь появляется и говорит тебе снять шлем. Ты его послушно снимаешь и понимаешь, что ты был всего лишь тестером гиперреалистичной игрушки, которая вводит тебя в кому и транслирует тебе игру с помощью специальной установки, в которой ты лежал... это всё было игрой.\nСпасибо за прохождение!\nНад этой игрушкой старался ученик-студент группы 25ИС11Д, потратил 2.5 часа.");
         break;
     }
 
-    Console.WriteLine("\nВыбери действие:\n 1. Посмотреть под всей мебелью и за ней\n 2. Пойти вздремнуть на кровати\n 3. Попробовать найти хоть что-то, что может дать свет\n 4. Посмотреть под кровать");
+    Console.WriteLine("\nВыбери действие:");
+    Console.WriteLine("1. Посмотреть под всей мебелью и за ней");
+    Console.WriteLine("2. Пойти вздремнуть на кровати");
+    Console.WriteLine("3. Попробовать найти хоть что-то, что может дать свет / Посмотреть щиток");
+    Console.WriteLine("4. Посмотреть под кровать");
+
     string first = Console.ReadLine();
+    Console.WriteLine("------------------------------");
 
     switch (first)
     {
         case "1":
-            Console.WriteLine($"Прошерстив каждый уголок за каждой тумбочкой, ты находишь пару вещичек:\n Странный карандаш \n Конверт с загадкой, которую ты сразу разгадываешь!\n{name}, в инвентарь добавлены: 'Конверт' и 'Странный карандаш'");
+            Console.WriteLine($"Прошерстив каждый уголок за каждой тумбочкой, ты находишь пару вещичек:\n Странный карандаш \n Конверт с загадкой, которую ты сразу разгадываешь!\n{name}, Конверт и Странный карандаш добавлены в инвентарь");
+            karandash = true;
             check = false;
+            Console.WriteLine("------------------------------");
 
-            while (check == false)
+            while (!check)
             {
-                Console.WriteLine("Подумай \n 1. Попробовать нарисовать что-нибудь на стенах\n 2. Где то тут должна быть вентиляция!\n 3. Вернуться назад в своих размышлениях");
-                string two = Console.ReadLine();
+                Console.WriteLine("Подумай:");
+                int variant = 1;
+                int drawVariant = -1;
+                int ventVariant = -1;
+                int backVariant = -1;
 
-                second_art = true;
-
-                switch (two)
+                if (!narisoval)
                 {
-                    case "1":
-                        Console.WriteLine($"Взяв карандаш, ты рисуешь и понимаешь, что он материализует всё, что ты нарисуешь. Ты рисуешь дверь и, открыв её, попадаешь в кладовку. Там ты находишь конверт.\n{name}, 'Конверт' добавлен в инвентарь");
-                        first_art = true;
-                        check = false;
-                        break;
+                    drawVariant = variant;
+                    Console.WriteLine($"{variant}. Попробовать нарисовать что-нибудь на стенах");
+                    variant++;
+                }
 
-                    case "2":
-                        Console.WriteLine("Ты решил осмотреться ещё раз и заметил вентиляцию. Попробуй открыть её.");
+                ventVariant = variant;
+                Console.WriteLine($"{variant}. Где то тут должна быть вентиляция!");
+                variant++;
+
+                backVariant = variant;
+                Console.WriteLine($"{variant}. Вернуться назад в своих размышлениях");
+
+                string two = Console.ReadLine();
+                Console.WriteLine("------------------------------");
+
+                if (drawVariant != -1 && two == drawVariant.ToString() && !narisoval)
+                {
+                    Console.WriteLine($"Взяв карандаш, ты рисуешь и понимаешь, что он материализует всё, что ты нарисуешь. Ты рисуешь дверь и, открыв её, попадаешь в кладовку. Там ты находишь конверт.\n{name}, Конверт добавлен в инвентарь");
+                    konvert = true;
+                    narisoval = true;
+                }
+                else if (two == ventVariant.ToString())
+                {
+                    if (ventilyatsiyaOtkryta)
+                    {
+                        Console.WriteLine("Ты снова заглядываешь в вентиляцию, и видишь следующее:");
+                        Console.WriteLine("Здесь больше ничего нет, Только пыль и звук улицы");
+                        Console.WriteLine("------------------------------");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ты решил осмотреться и заметил вентиляцию. Попробуй открыть её.");
                         check2 = false;
-                        schetchik = 0;
+                        schetchik = 1;
                         Random chislo = new Random();
-                        int chislo2 = chislo.Next(1, 10);
-                        while (check2 == false)
-                        {
-                            schetchik++;
-                            int POPITka = first_art ? 4 : chislo2;
+                        int maxTry = chislo.Next(5, 10);
 
-                            if (schetchik >= POPITka)
+                        while (!check2)
+                        {
+                            int limit = lom ? 3 : maxTry;
+
+                            if (schetchik >= limit)
                             {
-                                Console.WriteLine($"С {POPITka}-го раза ты открыл вентиляцию с помощью лома, за вентиляцией ты находишь полотенце.\n{name}, 'Полотенце' добавлено в инвентарь");
-                                chetverty = true;
+                                string chemOtkryl = lom ? "лома" : "пустых рук";
+                                Console.WriteLine($"С {schetchik}-го раза ты открыл вентиляцию с помощью {chemOtkryl}, за вентиляцией ты находишь полотенце.\n{name}, Полотенце добавлено в инвентарь");
+                                polotentse = true;
+                                ventilyatsiyaOtkryta = true;
                                 check2 = true;
-                                check = false;
+                                Console.WriteLine("------------------------------");
                             }
                             else
                             {
                                 Console.WriteLine("Чет не получилось, мало каши ел. Попробуй ещё раз.");
                                 Console.WriteLine("Нажми Enter, чтобы попробовать снова...");
                                 Console.ReadLine();
-                                check = false;
+                                schetchik++;
+                                Console.WriteLine("------------------------------");
                             }
                         }
-                        break;
-
-                    case "3":
-                        check = true;
-                        break;
-
-                    default:
-                        Console.WriteLine("Отец не оценил твой выбор, попробуй ещё раз.");
-                        break;
+                    }
+                }
+                else if (two == backVariant.ToString())
+                {
+                    check = true;
+                }
+                else
+                {
+                    Console.WriteLine("Отец не оценил твой выбор, попробуй ещё раз.");
+                    Console.WriteLine("------------------------------");
                 }
             }
             break;
@@ -87,55 +140,102 @@ while (game == false)
         case "2":
             Console.WriteLine("Решив поспать, тебе приснился странный сон. И вдруг ты просыпаешься в своей домашней кровати, смотришь в окно, а там мирное обычное утро. Кто знает, может, это всё был сон...\nПоздравляем! Ты выбрался из комнаты. Молодец!");
             game = true;
+            Console.WriteLine("------------------------------");
             break;
 
         case "3":
-            Console.WriteLine("Ты находишь батарейки и фольгу. Хм, можно сделать зажигалку... не зря смотрел ютубчик. Чуть позже ты разжигаешь огонь и видишь провода, идущие к щитку.\n1. Попытаться открыть щиток\n2. Вернуться позже");
-            string three = Console.ReadLine();
-
-            switch (three)
+            if (!folgaIBatareykiNaydeny && !shitokVkluchen)
             {
-                case "1":
-                    if (first_art)
-                    {
-                        Console.WriteLine($"Ты рисуешь отверстие в щитке карандашом. Пробравшись внутрь, включаешь электричество. Что-то движется. Осмотревшись, ты видишь, что на полу появилась метла. Подойдя к ней, ты подбираешь её.\n{name} положил 'Метлу' в инвентарь");
-                        third_art = true;
-                    }
-                    else if (second_art)
-                    {
-                        Console.WriteLine($"Ты смог оторвать дверцу щитка. Включаешь электричество. Что-то движется. На полу появляется метла. Подбираешь её.\n{name} положил 'Метлу' в инвентарь");
-                        third_art = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("К сожалению, ты не смог открыть щиток без подручных инструментов. Попробуй что-нибудь поискать.");
-                    }
-                    break;
+                Console.WriteLine($"Ты находишь батарейки и фольгу.\n{name}, Фольга и батарейки добавлены в инвентарь");
+                folgaIBatareykiNaydeny = true;
 
-                case "2":
+                Console.WriteLine("Что делать дальше?");
+                int variantLight = 1;
+                int makeLighter = variantLight;
+                Console.WriteLine($"{variantLight}. Сделать зажигалку");
+                variantLight++;
+                int returnLater = variantLight;
+                Console.WriteLine($"{variantLight}. Вернуться позже");
+
+                string lightChoice = Console.ReadLine();
+                Console.WriteLine("------------------------------");
+
+                if (lightChoice == makeLighter.ToString() && !lighterSdelan)
+                {
+                    Console.WriteLine($"Вспоминая лайфхаки для выживания, ты берешь зажигалку, прикрепляешь фольгу к обоим концам батарейки и берёшь из подушки на кровати пух, поджигаешь его и видишь провода вокруг идущие к щитку...");
+                    lighterSdelan = true;
+                }
+                else
+                {
                     Console.WriteLine("Ты решил вернуться позже.");
-                    break;
-
-                default:
-                    Console.WriteLine("Неверный выбор, попробуй ещё раз.");
-                    break;
+                    Console.WriteLine("------------------------------");
+                }
             }
+
+            if (lighterSdelan || shitokVkluchen)
+            {
+                Console.WriteLine("Что делать дальше?");
+                int variantShitok = 1;
+                int checkShitok = variantShitok;
+                Console.WriteLine($"{variantShitok}. Осмотреть щиток");
+                variantShitok++;
+                int returnMain = variantShitok;
+                Console.WriteLine($"{variantShitok}. Вернуться в основное меню");
+
+                string choiceShitok = Console.ReadLine();
+                Console.WriteLine("------------------------------");
+
+                switch (choiceShitok)
+                {
+                    case var v when v == checkShitok.ToString():
+                        if (!shitokVkluchen)
+                        {
+                            if (konvert || karandash || lom)
+                            {
+                                Console.WriteLine($"Ты используешь инструмент и включаешь щиток. Свет загорается, осматриваясь, ты видишь, что на полу появилась метла. Подойдя к ней, ты подбираешь её.\n{name}, Метла добавлена в инвентарь");
+                                metla = true;
+                                shitokVkluchen = true;
+                            }
+                            else
+                            {
+                                Console.WriteLine("К сожалению, ты не смог открыть щиток без инструментов.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Все выглядит также, как и в прошлый раз. Свет включен");
+                        }
+                        Console.WriteLine("------------------------------");
+                        break;
+
+                    case var v when v == returnMain.ToString():
+                        break;
+
+                    default:
+                        Console.WriteLine("Неверный выбор, попробуй ещё раз.");
+                        Console.WriteLine("------------------------------");
+                        break;
+                }
+            }
+
             break;
 
         case "4":
-            if (chetverty_art)
+            if (!lom)
             {
-                Console.WriteLine("Заглянув под кровать снова, ты видишь белоснежную улыбку Рикардо Милоса.");
+                Console.WriteLine($"Ты заглядываешь под кровать и находишь Лом.\n{name}, Лом добавлен в инвентарь");
+                lom = true;
             }
             else
             {
-                Console.WriteLine($"Ты заглядываешь под кровать и находишь Лом.\n{name} В инвентарь добавлен 'Лом'");
-                chetverty_art = true;
+                Console.WriteLine("Заглянув под кровать снова, ты видишь белоснежную улыбку Рикардо Милоса.");
             }
+            Console.WriteLine("------------------------------");
             break;
 
         default:
             Console.WriteLine("Неверный выбор, попробуй ещё раз.");
+            Console.WriteLine("------------------------------");
             break;
     }
 }
